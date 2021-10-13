75f4b10df1499a4cfd733079d8a7324ebbda9bd5
TausifIqbal <111801044@smail.iitpkd.ac.in>
2021-10-13 18:43:00 +0530

    added user_plugin

---
+++ blocks/user_plugin/block_user_plugin.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for editing HTML block instances.
+ *
+ * @package   block_html
+ * @copyright 1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_user_plugin extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname', 'block_user_plugin');
+    }
+
+    function get_content() {
+        global $DB;
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+        $userstring.='<br>';
+        $users = $DB->get_records('user');
+        foreach($users as $user){
+//            $userstring .=$user->firstname . ' ' . $user->lastname . '<br>';
+            $userstring .=$user->firstname . '<br>';
+        }
+
+        $this->content = new stdClass;
+        $this->content->text= $userstring;
+
+        return $this->content;
+
+    }
+}
